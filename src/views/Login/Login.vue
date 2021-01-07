4fb40941ce0863c39ff72f3b6d798966d70933b3
geheng <951726863@qq.com>
2021-01-07 20:52:21 +0800

    登录页、校验规则

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -2,45 +2,23 @@
   <div id="login">
     <div class="login-wrap">
       <ul class="menu-tab">
-        <li
-          v-for="(item, index) in menuTab"
-          :class="{ current: item.current }"
-          :key="index"
-          @click="toggleMenu(item)"
-        >{{ item.text }}</li>
+        <li v-for="(item, index) in menuTab" :class="{ current: item.current }" :key="index" @click="toggleMenu(item)">
+          {{ item.text }}
+        </li>
       </ul>
       <!-- 表单 start -->
-      <el-form
-        :model="ruleForm"
-        status-icon
-        :rules="rules"
-        ref="ruleForm"
-        class="login-from"
-        size="medium"
-      >
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-from" size="medium">
         <el-form-item prop="userEmail" class="item-form">
           <label>邮箱</label>
           <el-input type="text" v-model="ruleForm.userEmail" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item prop="passWord" class="item-form">
           <label>密码</label>
-          <el-input
-            type="password"
-            v-model="ruleForm.passWord"
-            minlength="6"
-            maxlength="19"
-            autocomplete="off"
-          ></el-input>
+          <el-input type="password" v-model="ruleForm.passWord" minlength="6" maxlength="19" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item prop="passWords" v-if="menuTab[1].current" class="item-form">
           <label>确认密码</label>
-          <el-input
-            type="password"
-            v-model="ruleForm.passWords"
-            minlength="6"
-            maxlength="19"
-            autocomplete="off"
-          ></el-input>
+          <el-input type="password" v-model="ruleForm.passWords" minlength="6" maxlength="19" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item prop="code" class="item-form">
           <label>验证码</label>
@@ -66,82 +44,83 @@
   </div>
 </template>
 <script>
-import { validate_userEmail, validate_passWord } from "@/utils/validate.js"
+import { reactive } from '@vue/composition-api';
+import { validateUserEmail, validatePassWord, validatePassWords, checkCode } from '@/views/Login/login.js';
 export default {
-  name: "login",
-  data() {
-    // 验证码
-    var checkCode = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error('验证码不能为空'));
-      }
-      callback();
-    };
+  name: 'login',
+  setup(props, context) {
     // 邮箱验证
-    var validateUserEmail = (rule, value, callback) => {
-      if (value === '') { callback(new Error('用户名不能为空')); }
-      validate_userEmail(value) ? callback(new Error('用户名格式不正确')) : callback();
-    };
-    // 密码验证
-    var validatePassWord = (rule, value, callback) => {
-      if (value === '') { callback(new Error('密码不能为空')); }
-      validate_passWord(value) ? callback(new Error('密码格式错误')) : callback();
-    };
-    // 确认密码
-    var validatePassWords = (rule, value, callback) => {
-      if (value === '') { callback(new Error('密码不能为空')); }
-      this.ruleForm.passWord == value ? callback() : callback(new Error('两次密码不一致'));
-    }
-    return {
-      menuTab: [
-        { text: "登录", current: true },
-        { text: "注册", current: false }
-      ],
-      // 表单数据
-      ruleForm: {
-        userEmail: '',
-        passWord: '',
-        passWords: '',
-        code: ''
-      },
-      rules: {
-        userEmail: [
-          { validator: validateUserEmail, trigger: 'blur' }
-        ],
-        passWord: [
-          { validator: validatePassWord, trigger: 'blur' },
-        ],
-        passWords: [
-          { validator: validatePassWords, trigger: 'blur' }
-        ],
-        code: [
-          { validator: checkCode, trigger: 'blur' }
-        ]
-      }
-    };
-  },
-  methods: {
-    toggleMenu(data) {
-      this.menuTab.forEach(item => (item.current = false));
+    // let validateUserEmail = (rule, value, callback) => {
+    //   if (value === '') { callback(new Error('用户名不能为空')); }
+    //   validate_userEmail(value) ? callback(new Error('用户名格式不正确')) : callback();
+    // };
+    // // 密码验证
+    // let validatePassWord = (rule, value, callback) => {
+    //   if (value === '') { callback(new Error('密码不能为空')); }
+    //   validate_passWord(value) ? callback(new Error('密码格式错误')) : callback();
+    // };
+    // // 确认密码
+    // let validatePassWords = (rule, value, callback) => {
+    //   if (value === '') { callback(new Error('密码不能为空')); }
+    //   ruleForm.passWord == value ? callback() : callback(new Error('两次密码不一致'));
+    // };
+    // // 验证码
+    // let checkCode = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error('验证码不能为空'));
+    //   }
+    //   callback();
+    // };
+    const menuTab = reactive([
+      { text: '登录', current: true },
+      { text: '注册', current: false }
+    ]);
+    // 注册登录切换
+    const toggleMenu = data => {
+      menuTab.forEach(item => (item.current = false));
       data.current = true;
-    },
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
+    };
+    // 表单数据
+    const ruleForm = reactive({
+      userEmail: '',
+      passWord: '',
+      passWords: '',
+      code: ''
+    });
+    // 表单规则
+    const rules = reactive({
+      userEmail: [{ validator: validateUserEmail, trigger: 'blur' }],
+      passWord: [{ validator: validatePassWord, trigger: 'blur' }],
+      passWords: [{ validator: validatePassWords, trigger: 'blur' }],
+      code: [{ validator: checkCode, trigger: 'blur' }]
+    });
+    // 表单的提交处理
+    const submitForm = formName => {
+      context.refs[formName].validate(valid => {
         if (valid) {
           alert('submit!');
           // 清空表单
-          this.$refs[formName].resetFields();
+          context.refs[formName].resetFields();
         } else {
           console.log('error submit!!');
           return false;
         }
       });
-    },
-    resetForm(formName) {
-      // 清空表单
-      this.$refs[formName].resetFields();
-    }
-  }
+      // 重置表单
+      // let resetForm = (formName) => {
+      //   // 清空表单
+      //   context.refs[formName].resetFields();
+      // }
+    };
+    return {
+      menuTab,
+      ruleForm,
+      rules,
+      submitForm,
+      toggleMenu
+    };
+  },
+  methods: {}
 };
 </script>
 <style lang="scss" scoped>
@@ -150,7 +129,7 @@ export default {
   height: 100vh;
   justify-content: center;
   align-items: center;
-  background: url("../../assets/login/login.jpeg") no-repeat top center / cover;
+  background: url('../../assets/login/login.jpeg') no-repeat top center / cover;
   .menu-tab {
     li {
       display: inline-block;
